6302deaad32f62f8bac3ccc2df0ee439d83bf362
Ioseb Koplatadze <soso.kop@gmail.com>
2022-06-20 18:34:13 +0400

    add solid + tailwind

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { render } from 'solid-js/web';
+import './index.css';
+
+function App() {
+  return <p class="text-blue-600">Solid+Tailwind Parcel Starter</p>;
+}
+
+render(() => <App />, document.getElementById('root')!);
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
