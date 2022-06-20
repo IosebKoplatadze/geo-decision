368a680c7f0658b86dfc34ba98fd17cdb4876b63
Ioseb Koplatadze <soso.kop@gmail.com>
2022-06-20 20:14:59 +0400

    gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { render } from 'solid-js/web';
 import './index.css';
 
 function App() {
-  return <p class="text-blue-600">Solid+Tailwind Parcel Starter</p>;
+  return <p class="text-blue-600">Solid+Tailwind Parcel Starterssssssssssss</p>;
 }
 
 render(() => <App />, document.getElementById('root')!);
