3fdd68b99eaf6c71f06409cd2242cdc769b2803e
Ioseb Koplatadze <soso.kop@gmail.com>
2022-06-21 20:26:33 +0400

    footer text size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 const Footer = () => (
   <footer className="absolute top-0 right-0 h-full flex flex-col justify-center z-20 pointer-events-none mix-blend-difference">
-    <div className="relative -rotate-90 -right-28 text-[10px] text-amber-800">
+    <div className="relative -rotate-90 -right-24 sm:-right-28 text-[8px] sm:text-[10px] text-amber-800">
       <p>© 2022</p>
       <p>By Ioseb Koplatadze</p>
-      <i className="text-xs">I'm Georgian, and therefore I am European</i>
+      <i className="text-[10px] sm:text-xs">
+        I'm Georgian, and therefore I am European
+      </i>
     </div>
   </footer>
 );
