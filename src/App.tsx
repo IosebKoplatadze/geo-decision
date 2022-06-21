29adbb04b2bed07e6bfe4a8e6c4dda847e0bd6fc
Ioseb Koplatadze <soso.kop@gmail.com>
2022-06-21 18:52:58 +0400

    fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import Footer from './components/Footer';
 
 const App = () => (
-  <div className="h-screen flex flex-col justify-between font-mono overflow-hidden text-5xl  sm:text-8xl md:text-9xl">
-    <button className="h-1/2 bg-gradient-to-t from-[#91bdf5] to-indigo-900 text-white relative cursor-n-resize">
+  <div className="h-screen flex flex-col justify-between font-mono overflow-hidden">
+    <button className="h-1/2 bg-gradient-to-t from-[#91bdf5] to-indigo-900 text-white relative cursor-n-resize text-5xl  sm:text-8xl md:text-9xl">
       <span className="absolute -translate-x-1/2">EUROPE</span>
       <img
         src="./tbilisi.svg"
@@ -10,7 +10,7 @@ const App = () => (
       />
     </button>
 
-    <div className="relative h-52 ">
+    <div className="relative sm:h-52 h-32">
       <div class="ocean2 z-10">
         <div class="wave2"></div>
         <div class="wave2"></div>
@@ -24,10 +24,10 @@ const App = () => (
     </div>
 
     <button
-      className="h-1/2 relative bg-gradient-to-b from-[#531212] to-red-900 cursor-s-resize"
-      style="filter: contrast(3);"
+      className="h-1/2 relative bg-gradient-to-b from-[#531212] to-red-900 cursor-s-resize text-4xl  sm:text-7xl md:text-8xl"
+      style="filter: contrast(3); font-family: 'BARBEDWIRE', sans-serif;"
     >
-      <div className="relative text-[#FF0000]">
+      <div className="relative text-[#FF0000] ">
         <span className="absolute word -translate-x-1/2 bottom-0">
           OLIGARCHY
         </span>
