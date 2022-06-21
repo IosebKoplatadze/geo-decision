8b6b48105b702ab167fa53cfd5d35b787d72c5ce
Ioseb Koplatadze <soso.kop@gmail.com>
2022-06-21 16:07:01 +0400

    home page end

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,58 @@
-const App = () => {
-  return (
-    <div className="h-screen flex flex-col justify-between font-mono">
-      <div className="relative h-20 ">
-        <div class="ocean2 z-10">
-          <div class="wave2"></div>
-          <div class="wave2"></div>
-          <div class="wave2"></div>
-        </div>
+import Footer from './components/Footer';
+
+const App = () => (
+  <div className="h-screen flex flex-col justify-between font-mono overflow-hidden">
+    <button className="h-1/2 text-9xl bg-gradient-to-t from-[#91bdf5] to-indigo-900 text-white relative cursor-n-resize">
+      <span className="absolute -translate-x-1/2">EUROPE</span>
+      <img
+        src="./tbilisi.svg"
+        className="absolute top-0 rotate-180 scale-110"
+      />
+    </button>
+
+    <div className="relative h-52 ">
+      <div class="ocean2 z-10">
+        <div class="wave2"></div>
+        <div class="wave2"></div>
+        <div class="wave2"></div>
       </div>
-      <button className="h-1/2 text-9xl bg-[#0000FF] text-white">EUROPE</button>
-      <button
-        className="h-1/2 text-9xl bg-[#FF0000]"
-        style="filter: contrast(25);"
-      >
-        <div className="relative">
-          <span className="absolute word">OLIGARCHY</span>
-          <span className="absolute word">USSR</span>
-        </div>
-      </button>
-      <div className="relative h-20 ">
-        <div class="ocean z-10" style="filter: contrast(25);">
-          <div class="wave"></div>
-          <div class="wave"></div>
-          <div class="wave"></div>
-        </div>
+      <div class="ocean z-10" style="filter: contrast(25);">
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
       </div>
+    </div>
+
+    <button
+      className="h-1/2 text-9xl relative bg-gradient-to-b from-[#531212] to-red-900 cursor-s-resize"
+      style="filter: contrast(3);"
+    >
+      <div className="relative text-[#FF0000]">
+        <span className="absolute word -translate-x-1/2 bottom-0">
+          OLIGARCHY
+        </span>
+        <span className="absolute word -translate-x-1/2 bottom-0">USSR</span>
+      </div>
+      <img src="./war.svg" className="absolute -bottom-24" />
+    </button>
+    <marquee
+      className="w-full h-full absolute top-0 left-0 z-20 mix-blend-difference pointer-events-none"
+      behavior="alternate"
+      direction="down"
+      truespeed="true"
+      scrolldelay="30"
+    >
       <marquee
-        className="w-full h-full absolute top-0 left-0 z-20 mix-blend-difference pointer-events-none"
+        className=" text-white text-5xl -top-6"
         behavior="alternate"
-        direction="down"
         truespeed="true"
         scrolldelay="30"
       >
-        <marquee
-          className=" text-white text-5xl -top-6"
-          behavior="alternate"
-          truespeed="true"
-          scrolldelay="30"
-        >
-          <div className="relative text-[#FF00FF]">What is your decision?</div>
-        </marquee>
+        <div className="relative text-[#FF00FF]">What is your decision?</div>
       </marquee>
-    </div>
-  );
-};
+    </marquee>
+    <Footer />
+  </div>
+);
 
 export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => (
+  <footer className="absolute top-0 right-0 h-full flex flex-col justify-center z-20 pointer-events-none mix-blend-screen">
+    <div className="relative -rotate-90 -right-28 text-[10px] text-amber-800">
+      <p>© 2022</p>
+      <p>By Ioseb Koplatadze</p>
+      <i className="text-xs">I'm Georgian, and therefore I am European</i>
+    </div>
+  </footer>
+);
+
+export default Footer;
