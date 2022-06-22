75a7434e98728ff82f0271b8b216801d60f066e4
Ioseb Koplatadze <soso.kop@gmail.com>
2022-06-22 13:29:47 +0400

    refactor css

--- src/App.tsx
+++ src/App.tsx
@@ -8,45 +8,43 @@ const App: Component = () => {
 
   return (
     <div className="relative h-screen flex flex-col justify-between font-mono overflow-hidden min-h-[640px]">
-      <button className="h-1/2 bg-gradient-to-t from-[#91bdf5] to-indigo-900 text-white relative cursor-n-resize text-5xl  sm:text-7xl md:text-8xl text-sanet">
-        <span className="absolute -translate-x-1/2">{t('europe')}</span>
-        <img
-          src="./tbilisi.svg"
-          className="absolute top-0 rotate-180 scale-110"
-        />
+      <button className="h-1/2 cursor-n-resize ">
+        <div className="h-[90%] bg-gradient-to-t from-[#91bdf5] to-indigo-900 text-white text-5xl  sm:text-7xl md:text-8xl text-sanet">
+          <div className="h-2/3 w-full tbilisi bg-contain rotate-180 scale-110"></div>
+          <span>{t('europe')}</span>
+        </div>
+        <div class="ocean2">
+          <div class="wave2"></div>
+          <div class="wave2"></div>
+          <div class="wave2"></div>
+        </div>
       </button>
 
-      <div className="relative sm:h-52 h-32">
-        <div class="ocean2 z-10">
-          <div class="wave2"></div>
-          <div class="wave2"></div>
-          <div class="wave2"></div>
-        </div>
-        <div class="ocean z-10" style="filter: contrast(3);">
-          <div class="wave"></div>
-          <div class="wave"></div>
-          <div class="wave"></div>
-        </div>
-      </div>
-
       <button
-        className={
-          'h-1/2 relative bg-gradient-to-b from-[#531212] to-red-900 cursor-s-resize ' +
-          (locale() === 'ka'
-            ? 'text-sanet text-5xl  sm:text-7xl md:text-8xl'
-            : 'text-barbed text-4xl sm:text-6xl md:text-7xl')
-        }
+        className="h-1/2 cursor-s-resize"
         style="filter: contrast(3); line-height: inherit;"
       >
-        <div className="relative text-[#FF0000] ">
-          <span className="absolute word -translate-x-1/2 bottom-0">
-            {t('oligarchy')}
-          </span>
-          <span className="absolute word -translate-x-1/2 bottom-0">
-            {t('USSR')}
-          </span>
+        <div class="ocean" style="filter: contrast(3);">
+          <div class="wave"></div>
+          <div class="wave"></div>
+          <div class="wave"></div>
         </div>
-        <img src="./war.svg" className="absolute bottom-0" />
+        <div className="h-[90%] bg-gradient-to-b from-[#531212] to-red-900 flex flex-col-reverse">
+          <div className="h-2/3 w-full war bg-contain md:bg-cover"></div>
+          <div
+            className={
+              'relative text-[#FF0000] ' +
+              (locale() === 'ka'
+                ? 'text-sanet text-5xl  sm:text-7xl md:text-8xl'
+                : 'text-barbed text-4xl sm:text-6xl md:text-7xl')
+            }
+          >
+            <span className="word">{t('oligarchy')}</span>
+            <span className="absolute word -translate-x-1/2 left-1/2">
+              {t('USSR')}
+            </span>
+          </div>
+        </div>
       </button>
       <Bounce>{t('whatIsYourDecision?')}</Bounce>
       <Footer />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,12 +6,14 @@ const Footer = () => {
 
   return (
     <footer className="absolute top-0 right-0 h-full flex flex-col justify-center z-20  mix-blend-difference pointer-events-none">
-      <div className="relative -rotate-90 -right-24 sm:-right-28 text-amber-800 flex items-center ">
+      <div className="relative -rotate-90 -right-20 sm:-right-28 md:-right-32 text-amber-800 flex items-center ">
         <LangSwitcher />
-        <div className=" text-[8px] sm:text-[10px] ">
+        <div className="text-[6px] sm:text-[8px] md:text-[10px]">
           <p>© 2022</p>
           <p>{t('ByIosebKoplatadze')}</p>
-          <i className="text-[10px] sm:text-xs">{t('IAmGeorgian')}</i>
+          <i className="text-[8px] sm:text-[10px] md:text-xs">
+            {t('IAmGeorgian')}
+          </i>
         </div>
       </div>
     </footer>
