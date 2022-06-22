15ae79c5db5b85f9d9f8cdd4d79db09a246ef11b
Ioseb Koplatadze <soso.kop@gmail.com>
2022-06-22 14:11:29 +0400

    first animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import { useI18n } from '@solid-primitives/i18n';
-import { Component } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 import Bounce from './components/Bounce';
 import Footer from './components/Footer';
 
 const App: Component = () => {
   const [t, { locale }] = useI18n();
+  const [side, setSide] = createSignal();
+
+  const onSideChange = ({ currentTarget }) => {
+    setSide(Number(currentTarget.name));
+  };
 
   return (
     <div className="relative h-screen flex flex-col justify-between font-mono overflow-hidden min-h-[640px]">
-      <button className="h-1/2 cursor-n-resize ">
+      <button
+        name="1"
+        onClick={onSideChange}
+        className={
+          'cursor-n-resize transition-all ease-linear ' +
+          (side() === 1 ? 'h-[90%]' : side() === -1 ? 'h-[10%]' : 'h-1/2')
+        }
+      >
         <div className="h-[90%] bg-gradient-to-t from-[#91bdf5] to-indigo-900 text-white text-5xl  sm:text-7xl md:text-8xl text-sanet">
-          <div className="h-2/3 w-full tbilisi bg-contain lg:bg-cover rotate-180 scale-110"></div>
+          {side() !== -1 && (
+            <div className="h-2/3 w-full tbilisi bg-contain lg:bg-cover rotate-180 scale-110"></div>
+          )}
           <span>{t('europe')}</span>
         </div>
         <div class="ocean2">
@@ -21,8 +35,13 @@ const App: Component = () => {
       </button>
 
       <button
-        className="h-1/2 cursor-s-resize"
+        className={
+          'cursor-s-resize transition-all ease-linear ' +
+          (side() === -1 ? 'h-[90%]' : side() === 1 ? 'h-[10%]' : 'h-1/2')
+        }
         style="filter: contrast(3); line-height: inherit;"
+        name="-1"
+        onClick={onSideChange}
       >
         <div class="ocean" style="filter: contrast(3);">
           <div class="wave"></div>
@@ -30,7 +49,9 @@ const App: Component = () => {
           <div class="wave"></div>
         </div>
         <div className="h-[90%] bg-gradient-to-b from-[#531212] to-red-900 flex flex-col-reverse">
-          <div className="h-2/3 w-full war bg-contain md:bg-cover"></div>
+          {side() !== 1 && (
+            <div className="h-2/3 w-full war bg-contain md:bg-cover"></div>
+          )}
           <div
             className={
               'relative text-[#FF0000] ' +
--- src/components/LangSwitcher.tsx
+++ src/components/LangSwitcher.tsx
@@ -3,16 +3,16 @@ import { useI18n } from '@solid-primitives/i18n';
 const LangSwitcher = () => {
   const [_, { locale }] = useI18n();
 
-  const langChangeHandler = ({ target }) => {
+  const onLangChange = ({ target }) => {
     locale(target.name);
   };
   return (
     <div className="z-30 rotate-90 pointer-events-auto">
-      <button name="en" onClick={langChangeHandler}>
+      <button name="en" onClick={onLangChange}>
         EN
       </button>
       |
-      <button className="text-sanet" name="ka" onClick={langChangeHandler}>
+      <button className="text-sanet" name="ka" onClick={onLangChange}>
         ქა
       </button>
     </div>
