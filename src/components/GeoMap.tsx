2b5eaefb908299c3012675d5eac21064896287c8
Ioseb Koplatadze <soso.kop@gmail.com>
2022-06-24 15:31:55 +0400

    some map improvments

--- src/components/GeoMap.tsx
+++ src/components/GeoMap.tsx
@@ -20,29 +20,27 @@ const GeoMap = (props: { decision: Decision }) => {
 
   return (
     <div
-      className={`flex flex-col items-center absolute w-full h-full ${
+      className={`flex flex-col items-center relative h-1/2 ${
         props.decision === Decision.Europe ? '-top-1/2' : 'top-1/2'
       }`}
     >
-      <div className="flex justify-center absolute top-1/2  h-full w-full pointer-events-none">
+      <div className="flex justify-center absolute h-full pointer-events-none">
         <Flip
           className={`mix-blend-color-dodge text-xs xs:text-xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl ${
             props.decision === Decision.Oligarchy && 'oligarchy'
           } ${
             props.decision === Decision.Europe
               ? 'translate-y-1/2'
-              : '-translate-y-1/2'
+              : 'translate-y-1/2'
           }`}
         >
           {year()}
         </Flip>
       </div>
       <div
-        className={`map ${
-          props.decision === Decision.Europe
-            ? 'translate-y-1/2'
-            : '-translate-y-1/2'
-        }`}
+        className={`map absolute h-full 
+        
+        `}
       ></div>
     </div>
   );
